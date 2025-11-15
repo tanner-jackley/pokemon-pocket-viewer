1a80bc9314ee43babec00591923772236e44bdb1
Tanner Jackley <tannerjackley@gmail.com>
2025-11-15 13:03:18 -0600

    get all cards from a set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,18 @@
 import "../css/Home.css";
 import CardCard from "../components/CardCard.jsx";
-import { getCard } from "../services/api.js"
+import { getCard, getCardsFromSet } from "../services/api.js";
 
-const card = await getCard('A1-150');
-  // id: "A1-001",
-  // name: "Bulbasaur",
-  // rarity: "One Diamond",
-  // description:
-  //   "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
-
+const cards = await getCardsFromSet("A1");
 
 function Home() {
   return (
     <>
       <h1>Pokémon Pocket Viewer</h1>
-      <CardCard card={card} />
+      <div className="card-grid">
+        {cards.map((card) => (
+          <CardCard key={card.id} card={card} />
+        ))}
+      </div>
     </>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,23 @@
-import TCGdex from '@tcgdex/sdk'
+import TCGdex from "@tcgdex/sdk";
 
-const tcgdex = new TCGdex('en');
+const tcgdex = new TCGdex("en");
 
 export const getCard = async (cardId) => {
-  // Retrieve Furret from the Darkness Ablaze Set
   const card = await tcgdex.card.get(cardId);
-  console.log(card);
   return card;
+};
+
+export const getSet = async (setId) => {
+  const set = await tcgdex.set.get(setId);
+  return set;
+};
+
+export const getCardsFromSet = async (setId) => {
+  const cards = [];
+  const set = await getSet(setId);
+  set.cards.forEach(async (card) => {
+    cards.push(await getCard(card.id));
+  });
+  console.log(cards);
+  return cards;
 };
