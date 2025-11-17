d569e057a86dd1305359a58032f4ad31db9ef073
Tanner Jackley <tannerjackley@gmail.com>
2025-11-17 13:54:50 -0600

    Card only shows card img

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -2,7 +2,7 @@ import IndividualCard from "./IndividualCard";
 
 function CardGrid({ cards }) {
   return (
-    <div className="card-grid">
+    <div className="set-content">
       {cards.map((card) => (
         <IndividualCard key={card.id} card={card} />
       ))}
--- src/components/IndividualCard.jsx
+++ src/components/IndividualCard.jsx
@@ -1,15 +1,21 @@
+import "../css/IndividualCard.css";
+
 function IndividualCard({ card }) {
   return (
-    <div>
-      <div>
-        <img src={`${card.image}/low.png`} alt={card.name}></img>
-      </div>
+    <div className="individual-card">
+      {/* <div> */}
+      <img
+        src={`${card.image}/low.png`}
+        className="card-image"
+        alt={card.name}
+      ></img>
+      {/* </div>
       <div>
         <h3>{card.name}</h3>
         <p>{card.id}</p>
         <p>{card.description}</p>
         <p>{card.rarity}</p>
-      </div>
+      </div> */}
     </div>
   );
 }
